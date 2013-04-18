2407da8bd47128a97f0c8865ab79d9830ee4d456
lee52547 <lee52547@gmail.com>
2013-04-19 01:35:50 +0800

    github commit test

---
+++ src/main/java/org/ziyue/anaylzer/IAnaylzer.java
@@ -0,0 +1,14 @@
+package org.ziyue.anaylzer;
+
+/**
+ * 字符串字频分析类
+ * Created with IntelliJ IDEA.
+ * User: Jale
+ * Date: 13-4-19
+ * Time: 上午1:34
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IAnaylzer {
+
+    void  anaylze(String s);
+}
