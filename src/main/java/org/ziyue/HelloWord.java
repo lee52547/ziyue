b68ce6776e4a36f52c49a31303bb1eca1092aa44
lee52547 <lee52547@gmail.com>
2013-04-19 00:59:45 +0800

    The 1st Commit Test

---
+++ src/main/java/org/ziyue/HelloWord.java
@@ -0,0 +1,17 @@
+package org.ziyue;
+
+/**
+ * Hello Word
+ * Created with IntelliJ IDEA.
+ * User: Jale
+ * Date: 13-4-18
+ * Time: 下午11:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWord {
+
+    public static void main(String args[]) {
+        System.out.println("Fuck China.P.R!!");
+    }
+
+}
---
+++ src/main/java/org/ziyue/StringReader.java
@@ -0,0 +1,12 @@
+package org.ziyue;
+
+/**
+ * 字符串读取
+ * Created with IntelliJ IDEA.
+ * User: Jale
+ * Date: 13-4-19
+ * Time: 上午12:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringReader {
+}
